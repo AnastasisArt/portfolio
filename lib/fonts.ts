e2008963a395cac30873b15317b0f7e245579af0
Anastasis <vojkic.anastasia@gmail.com>
2026-02-25 13:48:35 +0100

    Fonts added

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,24 +1,15 @@
-import localFont from "next/font/local";
+import { Turret_Road, Work_Sans } from "next/font/google";
 
-export const futura = localFont({
-  src: [
-    { path: "../../public/fonts/FuturaCyrillicLight.ttf", weight: "300" },
-    { path: "../../public/fonts/FuturaCyrillicBook.ttf", weight: "400" },
-    { path: "../../public/fonts/FuturaCyrillicMedium.ttf", weight: "500" },
-    { path: "../../public/fonts/FuturaCyrillicDemi.ttf", weight: "600" },
-    { path: "../../public/fonts/FuturaCyrillicBold.ttf", weight: "700" },
-    { path: "../../public/fonts/FuturaCyrillicExtraBold.ttf", weight: "800" },
-    { path: "../../public/fonts/FuturaCyrillicHeavy.ttf", weight: "900" },
-  ],
-  variable: "--font-futura",
+export const turret = Turret_Road({
+  subsets: ["latin"],
+  weight: ["700", "800"],
+  variable: "--font-turret",
   display: "swap",
 });
 
-export const workSans = localFont({
-  src: [
-    { path: "../../public/fonts/WorkSansVariableFontwght.ttf", weight: "100 900" },
-    { path: "../../public/fonts/WorkSansItalicVariableFontwght.ttf", weight: "100 900", style: "italic" },
-  ],
+export const workSans = Work_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-work",
   display: "swap",
 });

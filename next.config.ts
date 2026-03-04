d1a5bdef1ccda86fed102ad8714d00627101f01a
Anastasis <vojkic.anastasia@gmail.com>
2026-03-04 18:30:17 +0100

    fix: redirect old dev routes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  //temporary redirect for recruiters
+  async redirects() {
+    return [
+      {
+        source: "/fr/dev",
+        destination: "/fr",
+        permanent: true,
+      },
+      {
+        source: "/dev",
+        destination: "/fr",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

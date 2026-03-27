ed4b3062ebac413ad62fb7b48be9010639872a49
Anastasis <vojkic.anastasia@gmail.com>
2026-03-27 06:55:16 +0100

    add: chevronDown icon

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,12 +1,15 @@
-import { FaGithub, FaLinkedin, FaAt, FaDiscord, FaPlay, FaPause } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaAt, FaDiscord, FaPlay, FaPause, FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 export const ICONS = {
   github: FaGithub,
   linkedin: FaLinkedin,
   mail: FaAt,
   discord: FaDiscord,
+
+  //ui
   play: FaPlay,
   pause: FaPause,
+  chevronDown: FaChevronDown,
 } as const;
 
 export type IconKey = keyof typeof ICONS;

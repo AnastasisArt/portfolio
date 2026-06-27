15aefad598378f0e5d0e39f43ef2f663f5aebcae
Anastasis <vojkic.anastasia@gmail.com>
2026-06-27 02:48:41 +0200

    updated: last update date for project

--- constants/projects.ts
+++ constants/projects.ts
@@ -2,7 +2,7 @@ export const PROJECTS = ['inprogress', 'ntarchi', 'agendapp', 'swapitup'] as con
 
 export type ProjectSlug = (typeof PROJECTS)[number];
 export const PROJECT_LAST_MODIFIED: Partial<Record<ProjectSlug, string>> = {
-  inprogress: "2026-05-04",
+  inprogress: "2026-06-27",
   agendapp: "2026-05-04",
   ntarchi: "2026-05-16",
 };
